5c4f7ea10032f807326092e9be857d2327fa8353
muhammaddzakiardiansyah <muhammaddzakiardiansyah811@gmail.com>
2023-05-20 12:21:16 +0700

    update

--- game/script.js
+++ game/script.js
@@ -3,6 +3,13 @@
 function hide() {
     const popupIntro = document.querySelector('.popupIntro');
     popupIntro.style.display = 'none';
+    document.querySelector('.startGame').style.display = 'block';
+    // setTimeout( () => {
+    //     setTimer();
+    //     setVirusMoving();
+    // }, 50);
+    setTimer();
+    setVirusMoving();
 }
 
 // function tools game
@@ -17,10 +24,12 @@ function quit() {
 
 // function timer
 
-function setTimer(timer) {
-    let minute = 0;
-    let second = 0;
+let interval;
+let minute = 0;
+let second = 0;
 
+function setTimer() {
+    const timer = document.getElementById('timer');
     setInterval(() => {
         second++
         if(second === 60) {
@@ -31,33 +40,80 @@ function setTimer(timer) {
         const setMinute = minute < 10 ? '0' + minute : minute;
         const setSecond = second < 10 ? '0' + second : second;
 
-        timer.textContent = setMinute + ':' + setSecond;
+        timer.innerText = setMinute + ':' + setSecond;
     }, 1000);
 
 }
 
-window.onload = () => {
-    const timer = document.getElementById('timer');
-    setTimer(timer);
-}
+// window.onload = () => {
+//     const timer = document.getElementById('timer');
+//     setTimer(timer);
+// }
 
 // function username game
 
 let usernameGame = [];
-const username = document.getElementById('username').value;
-console.log(username)
+
+function showUser() {
+    const player = document.querySelector('.player');
+    player.innerText = '';
+
+    for(i = 0; i < usernameGame.length; i++) {
+        player.innerText = usernameGame[0];
+    }
+}
+
+function createUser() {
+    const username = document.querySelector('input[name=username]');
+    usernameGame.push(username.value);
+    showUser()
+}
+
+
+// function get random array
+
+function getRandomItem(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const item = arr[randomIndex];
+    return item;
+}
 
 // function setVirusMoving
 
 function setVirusMoving() {
     const virus = document.getElementById('virus');
+    const number = [3, 85, 165, 240];
+    // virus moving
     setTimeout( () => {
-    virus.style.top = (parseInt(virus.style.top.replace('px', '')) + 1) + 'px';
-        if(parseInt(virus.style.top.replace('px', '')) > 600) {
-            virus.style.top = '-100px';
+    virus.style.marginTop = (parseInt(virus.style.marginTop.replace('px', '')) + 1) + 'px';
+    if(parseInt(virus.style.marginTop.replace('px', '')) > 430) {
+        virus.style.marginLeft = getRandomItem(number) + 'px';
+        virus.style.marginTop = '-70px';
+        //document.getElementById('fail').innerText = (parseInt(document.getElementById('fail').innerText)) + 1;
+        if((document.getElementById('fail').innerText = (parseInt(document.getElementById('fail').innerText)) + 1) > 10) {
+            alert(`Game over!! fail : ${document.getElementById('fail').innerText} score : ${document.getElementById('score').innerText} timer : ${document.getElementById('timer').innerText}`)
+            document.querySelector('.container').style.display = 'none';
+            document.getElementById('fail').innerText = '0';
         }
+    }
         setVirusMoving();
-    }, 1);
+    }, 2);
 }
 
-setVirusMoving()
+
+
+
+// function keydown
+const blok4 = document.querySelector('.blok-4');
+console.log(blok4)
+window.addEventListener('keydown', (e) => {
+    if(e.key == 'k') {
+        if((parseInt(virus.style.marginTop.replace('px', '')) > 300)) {
+            const number = [3, 85, 165, 240];
+            virus.style.marginTop = '-70px';
+            virus.style.marginLeft = getRandomItem(number) + 'px';
+            document.getElementById('score').innerText = (parseInt(document.getElementById('score').innerText)) + 1;
+        }
+    }
+})
+
