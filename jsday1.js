f8b42aa2a1a19cebe28e97063fba4cc51b7033e0
muhammaddzakiardiansyah <muhammaddzakiardiansyah811@gmail.com>
2023-05-28 10:25:35 +0700

    week 3

--- jsday1.js
+++ jsday1.js
@@ -23,3 +23,86 @@ function findIndex(array, number) {
 console.log(findIndex([1,2,3,4,5], 3));
 console.log(findIndex([1,2,3,4,5], 6));
 console.log(findIndex([1,2,3,4,5], 4));
+
+
+// <?php
+
+// namespace App\Http\Controllers;
+
+// use App\Models\User;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Hash;
+
+// class AuthController extends Controller
+// {
+//     public function register(Request $request)
+//     {
+//         $validatedData = $request->validate([
+//             'name' => 'required|string',
+//             'email' => 'required|email|unique:users',
+//             'password' => 'required|string|min:6',
+//         ]);
+
+//         $user = User::create([
+//             'name' => $validatedData['name'],
+//             'email' => $validatedData['email'],
+//             'password' => Hash::make($validatedData['password']),
+//         ]);
+
+//         return response()->json(['message' => 'User registered successfully']);
+//     }
+
+//     public function login(Request $request)
+//     {
+//         $credentials = $request->only('email', 'password');
+
+//         $user = User::where('email', $credentials['email'])->first();
+
+//         if (!$user || !Hash::check($credentials['password'], $user->password)) {
+//             return response()->json(['message' => 'Invalid credentials'], 401);
+//         }
+
+//         // Generate token secara manual
+//         $token = base64_encode($user->email . ':' . $credentials['password']);
+
+//         return response()->json(['token' => $token]);
+//     }
+
+//     public function logout(Request $request)
+//     {
+//         // Implementasikan sesuai kebutuhan Anda, misalnya menghapus token yang disimpan oleh klien
+//         return response()->json(['message' => 'Logout successful']);
+//     }
+// }
+
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Route;
+
+// Route::post('register', 'AuthController@register');
+// Route::post('login', 'AuthController@login');
+
+// Route::group(['middleware' => 'auth'], function () {
+//     Route::post('logout', 'AuthController@logout');
+//     Route::get('user', function (Request $request) {
+//         return $request->user();
+//     });
+// });
+
+// protected $routeMiddleware = [
+//     // ...
+//     'auth' => \App\Http\Middleware\Authenticate::class,
+// ];
+
+
+//$hashedPassword = Hash::make(md5($plaintext));
+
+// $plaintext = 'password123';
+// $hashedPassword = '$2y$10$Pj12tB8I7l9vyeZ35UV8DOQ6MldouUjR.9kAEtXDE4r5kMfA2sjA6';
+
+// if (Hash::check(md5($plaintext), $hashedPassword)) {
+//     // Password cocok
+// } else {
+//     // Password tidak cocok
+// }
+
+// https://youtube.com/playlist?list=PL0Zuz27SZ-6PRCpm9clX0WiBEMB70FWwd
